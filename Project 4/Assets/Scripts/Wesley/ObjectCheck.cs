6b6aa55487c433aa4eed1349561c0dc2fbf4ef63
Isoply <50321050+Isoply@users.noreply.github.com>
2020-04-07 13:55:33 +0200

    Added A to B movement

---
+++ Project 4/Assets/Scripts/Wesley/ObjectCheck.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCheck : MonoBehaviour
+{
+    BasicMovement basicMovement;
+
+    [HideInInspector]public bool isMovingAway = false;
+
+    private void Start()
+    {
+        basicMovement = this.transform.parent.GetComponent<BasicMovement>();
+    }
+
+    private void Update()
+    {
+        if (isMovingAway)
+        {
+            basicMovement.Movement("Up");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collission)
+    {
+        if (collission.transform.parent.CompareTag("Prop"))
+        {
+            ChangeState();
+        }
+        else if (collission.transform.parent.gameObject == basicMovement.movePosition)
+        {
+            basicMovement.movePosition = null;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collission)
+    {
+        if (collission.transform.parent.CompareTag("Prop"))
+        {
+            ChangeState();
+        }
+    }
+
+    void ChangeState()
+    {
+
+        isMovingAway = !isMovingAway;
+        basicMovement.isConstructed = !basicMovement.isConstructed;
+    }
+}
---
+++ Project 4/Assets/Scripts/Wesley/Variables/MovementKeys.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementKeys : MonoBehaviour
+{
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+
+    public void SetKeys(KeyCode upKey, KeyCode downKey, KeyCode leftKey, KeyCode rightKey)
+    {
+        up = upKey;
+        down = downKey;
+        left = leftKey;
+        right = rightKey;
+    }
+}
