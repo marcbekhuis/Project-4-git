160a2990bcaa1edd13dc1bb1e286bfefd1813493
marc <marc@bekhuis.net>
2020-04-07 13:34:49 +0200

    hexagon calculator phase 1

---
+++ Project 4/Assets/Scripts/HexagonCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HexagonCalculator
+{
+    public static Vector2Int GetHexagonPosition(Vector2 position)
+    {
+        float col = position.x + (position.y - (position.y % 1)) / 2;
+        float row = position.y;
+        return new Vector2Int(Mathf.RoundToInt(col), Mathf.RoundToInt(row));
+    }
+}
