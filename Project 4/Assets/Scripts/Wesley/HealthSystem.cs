09000f85e496fbf141ebff05b3ff7a10079c4856
Isoply <50321050+Isoply@users.noreply.github.com>
2020-04-08 10:41:36 +0200

    Added a combat system
    and a few comments

---
+++ Project 4/Assets/Scripts/Wesley/HealthSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+
+    [SerializeField] int health = 100;
+    int maxHealth = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void TakeDamage(float damageTaken)
+    {
+        health -= (int)damageTaken;
+
+        if (health <= 0)
+        {
+            //death
+            Destroy(this.gameObject);
+        }
+    }
+}
