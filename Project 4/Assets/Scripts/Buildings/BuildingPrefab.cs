c7bcd0ccab4fad896b04fb93a2f7b113610324a6
marc <marc@bekhuis.net>
2020-04-14 09:49:15 +0200

    Verplaatste

---
+++ Project 4/Assets/Scripts/Buildings/BuildingPrefab.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Building", menuName = "ScriptableObjects/BuildingData")]
+public class BuildingPrefab : ScriptableObject
+{
+    public BuildingAction buildingAction;
+}
