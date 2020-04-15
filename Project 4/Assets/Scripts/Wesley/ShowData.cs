462d48fe451462e6a49e10377d6cdc8b7586a203
Isoply <50321050+Isoply@users.noreply.github.com>
2020-04-15 14:08:00 +0200

    cool thing data wow

---
+++ Project 4/Assets/Scripts/Wesley/ShowData.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowData : MonoBehaviour
+{
+    SaveSystem.SavedData saveData;
+    SaveSystem sSystem;
+    LineRenderer lr;
+
+    string playerName;
+    float timePlayed;
+
+    [SerializeField, Range(1,3)]int chosen = 1;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sSystem = this.GetComponent<SaveSystem>();
+        lr = this.GetComponent<LineRenderer>();
+
+        saveData = sSystem.LoadData("TestSave");
+
+        playerName = saveData.playerName;
+        timePlayed = saveData.timePlayed;
+
+        DisplayData(chosen);
+    }
+
+    public void DisplayData(int number)
+    {
+        switch (number)
+        {
+            case 1:
+                ShowSpecific(saveData.populationOverTime);
+                break;
+            case 2:
+                ShowSpecific(saveData.unitsOverTime);
+                break;
+            case 3:
+                ShowSpecific(saveData.citiesOverTime);
+                break;
+        }
+    }
+
+    void ShowSpecific(int[] list)
+    {
+        int timesRan = 0;
+        int position = 0;
+        timesRan = list.Length;
+        lr.positionCount = list.Length + 1;
+        foreach (var item in list)
+        {
+            float time = 0;
+            if (timesRan <= 0)
+            {
+                time = 0;
+            }
+            else
+            {
+                time = timePlayed / timesRan;
+            }
+            print(item + " | " + time);
+            timesRan--;
+            position++;
+
+            lr.SetPosition(position, new Vector3(time, item, 0));
+        }
+    }
+}
