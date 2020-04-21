b576273f3fececdb758520d2999f59e149fa31d1
MCM013 <mick.hofmeijer@gmail.com>
2020-04-21 09:39:18 +0200

    progress
    Added some folders, put some fonts in the fonts folder, made the script and pausemenu, just testing and optimizing it

---
+++ Project 4/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject[] EscMenu;
+
+    public void ButtonPause()
+    {
+        EscMenu[0].SetActive(true);
+        Time.timeScale = 0f;
+        EscMenu[1].SetActive(false);
+    }
+    public void ButtonEndMenu()
+    {
+        EscMenu[0].SetActive(false);
+        Time.timeScale = 1f;
+        EscMenu[1].SetActive(true);
+    }
+    public void ButtonStart()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+    }    
+    public void ButtonSave()
+    {
+        Debug.Log("SAVE"); 
+    }
+}
+
